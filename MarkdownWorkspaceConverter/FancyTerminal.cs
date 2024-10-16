f6e187e788159100783681cd7065784adb3e513d
timpaasche <tim.paasche@outlook.de>
2024-10-16 07:04:01 +0200

    Add project files.

---
+++ MarkdownWorkspaceConverter/FancyTerminal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkdownWorkspaceConverter;
+public static class FancyTerminal {
+
+    private const char _borderChar = ' ';
+    private const int _borderLength = 110;
+
+    public static void PrintHeader(string task, string location) {
+        location = " [ " + location + " ]";
+        task = $"{task} ";
+        int countOfDashes = _borderLength - location.Length;
+        Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
+        Console.WriteLine(new string('-', _borderLength));
+    }
+
+    public static void PrintSuccess(string task, string location) {
+        location = " [ " + location + " ]";
+        task = $"{task} ";
+        int countOfDashes = _borderLength - location.Length;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
+        Console.ResetColor();
+    }
+
+    public static void PrintInfo(string task, string location) {
+        location = " [ " + location + " ]";
+        task = $"{task} ";
+        int countOfDashes = _borderLength - location.Length;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
+        Console.ResetColor();
+    }
+
+    public static void PrintWarning(string task, string location) {
+        location = " [ " + location + " ]";
+        task = $"{task} ";
+        int countOfDashes = _borderLength - location.Length;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
+        Console.ResetColor();
+    }
+
+    public static void PrintError(string task, string location) {
+        location = " [ " + location + " ]";
+        task = $"{task} ";
+        int countOfDashes = _borderLength - location.Length;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
+        Console.ResetColor();
+    }
+}
---
+++ MarkdownWorkspaceConverter/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace MarkdownWorkspaceConverter {
+    internal class Program {
+        private static void Main(string[] args) {
+#if DEBUG
+            args = new[] { @"C:\Users\TLP-PC\Desktop\Envirement_for_testing\MarkdownWorkspace", @"C:\Users\TLP-PC\Desktop\Envirement_for_testing\MarkdownWorkspace_Copy" };
+#endif
+            if (args.Length != 2) {
+                Console.WriteLine("Usage: MarkdownWorkspaceConverter <srcDir> <dstDir>");
+                return;
+            }
+
+            string srcDir = args[0];
+            string dstDir = args[1];
+
+            FancyTerminal.PrintHeader("TASK", "FILE / PATH");
+
+            if (!Directory.Exists(srcDir)) {
+                FancyTerminal.PrintError("Source directory does not exist", srcDir);
+                throw new DirectoryNotFoundException();
+            }
+
+            if (!Directory.Exists(dstDir)) {
+                CreateDirectoryAndLog(dstDir);
+            }
+
+            foreach (string srcFile in Directory.EnumerateFiles(srcDir, "*.md", SearchOption.AllDirectories)) {
+                CopyFileAndLog(srcFile, dstDir);
+            }
+        }
+
+        private static void CreateDirectoryAndLog(string dir) {
+            ArgumentNullException.ThrowIfNull(dir);
+
+            try {
+                Directory.CreateDirectory(dir);
+                FancyTerminal.PrintSuccess("Directory created", dir);
+            } catch (Exception) {
+                FancyTerminal.PrintError("Creation of directory failed", dir);
+            }
+        }
+
+        private static void CopyFileAndLog(string srcFile, string dstDir) {
+            ArgumentNullException.ThrowIfNull(srcFile);
+
+            string fileName = Path.GetFileName(srcFile);
+
+            try {
+                File.Copy(srcFile, Path.Combine(dstDir, fileName), true);
+                FancyTerminal.PrintSuccess("File copied", fileName);
+            } catch (Exception) {
+                FancyTerminal.PrintError("Copying of file failed", fileName);
+            }
+        }
+    }
+}
