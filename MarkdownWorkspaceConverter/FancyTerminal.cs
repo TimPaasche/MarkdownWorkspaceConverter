09f02f9ae95d759bfb81a3703a1f91989942562e
Tim Paasche <tim.paasche.w@sew-eurodrive.de>
2024-10-17 08:06:50 +0200

    added stuff

--- MarkdownWorkspaceConverter/FancyTerminal.cs
+++ MarkdownWorkspaceConverter/FancyTerminal.cs
@@ -5,50 +5,71 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MarkdownWorkspaceConverter;
-public static class FancyTerminal {
+public static class FancyTerminal
+{
 
     private const char _borderChar = ' ';
     private const int _borderLength = 110;
 
-    public static void PrintHeader(string task, string location) {
+    public static void PrintHeader(string task, string location)
+    {
         location = " [ " + location + " ]";
         task = $"{task} ";
-        int countOfDashes = _borderLength - location.Length;
+        int countOfDashes = _borderLength > location.Length
+            ? _borderLength - location.Length
+            : 0;
+
         Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
         Console.WriteLine(new string('-', _borderLength));
     }
 
-    public static void PrintSuccess(string task, string location) {
+    public static void PrintSuccess(string task, string location)
+    {
         location = " [ " + location + " ]";
         task = $"{task} ";
-        int countOfDashes = _borderLength - location.Length;
+        int countOfDashes = _borderLength > location.Length
+            ? _borderLength - location.Length
+            : 0;
+
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
         Console.ResetColor();
     }
 
-    public static void PrintInfo(string task, string location) {
+    public static void PrintInfo(string task, string location)
+    {
         location = " [ " + location + " ]";
         task = $"{task} ";
-        int countOfDashes = _borderLength - location.Length;
+        int countOfDashes = _borderLength > location.Length
+            ? _borderLength - location.Length
+            : 0;
+
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
         Console.ResetColor();
     }
 
-    public static void PrintWarning(string task, string location) {
+    public static void PrintWarning(string task, string location)
+    {
         location = " [ " + location + " ]";
         task = $"{task} ";
-        int countOfDashes = _borderLength - location.Length;
+        int countOfDashes = _borderLength > location.Length
+            ? _borderLength - location.Length
+            : 0;
+
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
         Console.ResetColor();
     }
 
-    public static void PrintError(string task, string location) {
+    public static void PrintError(string task, string location)
+    {
         location = " [ " + location + " ]";
         task = $"{task} ";
-        int countOfDashes = _borderLength - location.Length;
+        int countOfDashes = _borderLength > location.Length
+            ? _borderLength - location.Length
+            : 0;
+
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"{task.PadRight(countOfDashes, _borderChar)}{location}");
         Console.ResetColor();
--- MarkdownWorkspaceConverter/Program.cs
+++ MarkdownWorkspaceConverter/Program.cs
@@ -1,58 +1,196 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
-namespace MarkdownWorkspaceConverter {
-    internal class Program {
-        private static void Main(string[] args) {
+namespace MarkdownWorkspaceConverter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
 #if DEBUG
-            args = new[] { @"C:\Users\TLP-PC\Desktop\Envirement_for_testing\MarkdownWorkspace", @"C:\Users\TLP-PC\Desktop\Envirement_for_testing\MarkdownWorkspace_Copy" };
+            args = [@"C:\Users\DEPAATIM\Desktop\Test\WIKI", @"C:\Users\DEPAATIM\Desktop\Test\WIKI-copy"];
+            //args = [@"C:\TFS\Repository\Inno_ICom.wiki", @"C:\Users\DEPAATIM\Desktop\Test\Inno_ICom.wiki_copy"];
 #endif
-            if (args.Length != 2) {
+            if (args.Length != 2)
+            {
                 Console.WriteLine("Usage: MarkdownWorkspaceConverter <srcDir> <dstDir>");
                 return;
             }
 
-            string srcDir = args[0];
-            string dstDir = args[1];
+            string inputDir = args[0];
+            string outputDir = args[1];
 
             FancyTerminal.PrintHeader("TASK", "FILE / PATH");
 
-            if (!Directory.Exists(srcDir)) {
-                FancyTerminal.PrintError("Source directory does not exist", srcDir);
+            if (!Directory.Exists(inputDir))
+            {
+                FancyTerminal.PrintError("Source directory does not exist", inputDir);
                 throw new DirectoryNotFoundException();
             }
 
-            if (!Directory.Exists(dstDir)) {
-                CreateDirectoryAndLog(dstDir);
+            if (!Directory.Exists(outputDir))
+            {
+                CreateDirectoryAndLog(outputDir);
             }
 
-            foreach (string srcFile in Directory.EnumerateFiles(srcDir, "*.md", SearchOption.AllDirectories)) {
-                CopyFileAndLog(srcFile, dstDir);
+            foreach (string srcFile in Directory.EnumerateFiles(inputDir, "*.md", SearchOption.AllDirectories))
+            {
+                string srcDir = Path.GetDirectoryName(srcFile);
+                string dstFile = BuildDstPath(inputDir, outputDir, srcFile);
+                string dstDir = Path.GetDirectoryName(dstFile);
+                CopyFileAndLog(srcFile, Path.GetDirectoryName(dstFile));
+                ConvertMdFileToUtf8(dstFile);
+                CreateLocalAttachments(srcDir, srcFile, dstDir, dstFile);
             }
+
         }
 
-        private static void CreateDirectoryAndLog(string dir) {
+        private static void CreateDirectoryAndLog(string dir)
+        {
             ArgumentNullException.ThrowIfNull(dir);
 
-            try {
+            try
+            {
                 Directory.CreateDirectory(dir);
                 FancyTerminal.PrintSuccess("Directory created", dir);
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 FancyTerminal.PrintError("Creation of directory failed", dir);
             }
         }
 
-        private static void CopyFileAndLog(string srcFile, string dstDir) {
+        private static void CopyFileAndLog(string srcFile, string dstDir)
+        {
             ArgumentNullException.ThrowIfNull(srcFile);
+
+            if (!Path.Exists(dstDir))
+            {
+                CreateDirectoryAndLog(dstDir);
+            }
 
             string fileName = Path.GetFileName(srcFile);
 
-            try {
+            try
+            {
                 File.Copy(srcFile, Path.Combine(dstDir, fileName), true);
                 FancyTerminal.PrintSuccess("File copied", fileName);
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 FancyTerminal.PrintError("Copying of file failed", fileName);
             }
         }
+
+        private static string BuildDstPath(string inputDir, string outputDir, string filePath)
+        {
+            return filePath.Replace(inputDir, outputDir);
+        }
+
+        private static void CreateLocalAttachments(string srcDir, string srcFile, string dstDir, string dstFile)
+        {
+            string attachmentDir = dstDir + @"\.attachment-" + Path.GetFileNameWithoutExtension(dstFile);
+            CreateDirectoryAndLog(attachmentDir);
+
+            string regexPattern = @"!\[.*?\]\((.*?)\)";
+            string markdownContent = File.ReadAllText(dstFile);
+
+            MatchCollection regexMatches = Regex.Matches(markdownContent, regexPattern);
+
+            List<(string oldPath, string newPath)> paths = [];
+            foreach (Match match in regexMatches)
+            {
+                if (!match.Success) { continue; }
+                string path = match.Groups[1].Value;
+
+                if (path.StartsWith("Http") || path.StartsWith("http")) { continue; }
+
+                FancyTerminal.PrintInfo("Found file refernce", path);
+                if (Path.IsPathRooted(path))
+                {
+                    if (!path.StartsWith('/') && !path.StartsWith('\\'))
+                    {
+                        FancyTerminal.PrintWarning("A path in *.md-file is absolute", dstFile);
+                    }
+
+                    string srcAttachmentPath = Path.Combine(srcDir, path.TrimStart('/').TrimStart('\\'));
+                    CopyFileAndLog(srcAttachmentPath, attachmentDir);
+                    string newPath = @".attachment-" + Path.GetFileNameWithoutExtension(dstFile) + @"\" + Path.GetFileName(path);
+                    FancyTerminal.PrintInfo("refactor link", newPath);
+                    paths.Add((path, newPath));
+                }
+                else
+                {
+                    string srcAttachmentPath = Path.Combine(srcDir, path);
+                    CopyFileAndLog(srcAttachmentPath, attachmentDir);
+                    string newPath = @".attachment-" + Path.GetFileNameWithoutExtension(dstFile) + @"\" + Path.GetFileName(path);
+                    FancyTerminal.PrintInfo("refactor link", newPath);
+                    paths.Add((path, newPath));
+                }
+            }
+            string content = File.ReadAllText(dstFile);
+            foreach ((string oldPath, string newPath) path in paths)
+            {
+                content = content.Replace(path.oldPath, path.newPath);
+            }
+            File.WriteAllText(dstFile, content);
+        }
+
+        private static void ConvertMdFileToUtf8(string mdFile)
+        {
+            string content = File.ReadAllText(mdFile, Encoding.Default);
+            content = FormatMarkdownHeadlines(content);
+            content = FormatMarkdownTables(content);
+            File.WriteAllText(mdFile, content, Encoding.UTF8);
+            FancyTerminal.PrintSuccess("*.md-file to utf8 converted", mdFile);
+        }
+
+        private static string FormatMarkdownHeadlines(string mdStream)
+        {
+            string pattern = @"(#+)([\w]+)";
+            string replacement = @"$1 $2";
+            string result = Regex.Replace(mdStream, pattern, replacement);
+
+            return result;
+        }
+
+
+        private static string FormatMarkdownTables(string mdStream)
+        {
+            // FIX MISSING '|' CHAR
+            //string patternMissingEndlineChar = @"^\|.*[^|\s]\s*$";
+            //MatchCollection result = Regex.Matches(mdStream, patternMissingEndlineChar, RegexOptions.Multiline);
+            //foreach (Match match in result)
+            //{
+
+            //    string newLine = $"{match.Groups[1].Value.TrimEnd()} |";
+            //    mdStream = mdStream.Replace(match.Groups[0].Value, newLine);
+            //}
+
+            // FIX SEPERATOR COUNT
+            string pattern = @"(\|[^\n]+\|\r?\n)((?:\|:?[-]+:?)+\|)";
+            MatchCollection regexMatches = Regex.Matches(mdStream, pattern);
+
+            foreach (Match match in regexMatches)
+            {
+                int countHeader = match.Groups[1].Value.Split('|').Length - 2; // Adjusted to get the correct count
+                FancyTerminal.PrintInfo(task: $"Count Columns (header): {countHeader}", location: "x");
+                int countSeparator = match.Groups[2].Value.Split('|').Length - 2; // Adjusted to get the correct count
+                FancyTerminal.PrintInfo(task: $"Count Columns (separator): {countSeparator}", location: "x");
+                for (int i = 0; i < countSeparator - countHeader; i++)
+                {
+                    var firstHalf = mdStream.Substring(0, match.Groups[2].Index);
+                    var secondHalf = mdStream.Substring(match.Groups[2].Index).TrimStart('|').TrimStart('-');
+
+                    mdStream = firstHalf + secondHalf;
+                }
+            }
+
+            return mdStream;
+        }
+
     }
 }
